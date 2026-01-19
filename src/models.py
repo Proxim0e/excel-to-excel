99ef6881303edd4173816e4ccb2b2385c7e61a62
Proxim0e <esan.ivan.rom@gmail.com>
2026-01-19 07:28:16 +0200

    create: added docs list

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
-
+from dataclasses import dataclass, field
+from typing import List, Optional, Tuple
 @dataclass
 class Participant:
     """
@@ -9,6 +10,8 @@ class Participant:
     name: str
     price_str: str  # Сырая строка цены с сайта (например, "30 833,35 MDL")
     price_val: Optional[float] = None  # Парсинговое число (например, 30833.35)
+    # Список всех документов: (имя_файла, ссылка)
+    docs: List[Tuple[str, str]] = field(default_factory=list)
 
 @dataclass
 class LotData:
